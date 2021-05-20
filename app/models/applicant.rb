fe74631d7436df3f6c4d731ba9f6416f66317465
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-20 17:37:46 -0600

    Update models

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,3 +1,6 @@
 class Applicant < ApplicationRecord
   has_one :address, dependent: :destroy
+  belongs_to :application, foreign_key: true
+
+  validates_associated :address
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,8 @@ class Application < ApplicationRecord
   has_one :applicant, dependent: :destroy
   has_many :pets
 
+  validates_associated :applicant
+  
+  validates :description, presence: true
+  validates :status, presence: true
 end
