df7fca13332edb6d467c312bcf34c2a4ae219be6
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-20 17:38:07 -0600

    Update migrations

--- db/migrate/20210520203759_create_addresses.rb
+++ db/migrate/20210520203759_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
       t.string :city
       t.string :state
       t.numeric :zipcode
-      t.belongs_to :applicant
+      t.belongs_to :applicant, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210520211546_create_applicants.rb
+++ db/migrate/20210520211546_create_applicants.rb
@@ -2,7 +2,7 @@ class CreateApplicants < ActiveRecord::Migration[5.2]
   def change
     create_table :applicants do |t|
       t.string :name
-      t.belongs_to :application
+      t.belongs_to :application, foreign_key: true
 
       t.timestamps
     end
