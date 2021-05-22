f4e203cc8051b0711d913f97c653b9513d15d4ab
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-22 00:39:35 -0600

    Implement model and view

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,15 @@ class Application < ApplicationRecord
 
   validates :description, presence: true
   validates :status, presence: true
+
+  def not_new
+    not applicant.nil?
+  end
+
+  def can_not_edit_description
+    return true if (applicant.nil? && pets.empty? && (status.nil? || status == 'In Progress'))
+    return true if (!applicant.nil? && !pets.empty? && status == 'Pending')
+    return true if (!applicant.nil? && pets.empty? && status == 'In Progress')
+    return false if (!applicant.nil? && !pets.empty? && status == 'In Progress')
+  end
 end
