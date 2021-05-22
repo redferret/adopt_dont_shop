f9ff153ee60773df34db87a1406296b2e6f86456
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-22 00:38:24 -0600

    Add model tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
-  before :all do
+  before :each do
     @application = FactoryBot.create(:application)
   end
 
   after :each do
-    Applicant.destroy_all
-  end
-
-  after :all do
     Application.destroy_all
   end
 
@@ -25,10 +21,38 @@ RSpec.describe Application, type: :model do
   end
 
   describe '#can_not_edit_description' do
-    it 'returns true if the applicant needs to select pets or app is new or status is pending' do
-      
+    it 'returns true if the applicant needs to select pets and app is new' do
+      @application.status = 'In Progress'
+      expect(@application.can_not_edit_description).to eq true
     end
 
-    it 'returns false if the applicant has pets selected and status is not pending'
+    it' returns true if the application is pending, pets are selected and app is not new' do
+      FactoryBot.create(:applicant, application: @application)
+      pet = FactoryBot.create(:pet, shelter: FactoryBot.create(:shelter))
+
+      @application.pets << pet
+      @application.description = Faker::Lorem.sentence
+      @application.status = 'Pending'
+
+      expect(@application.can_not_edit_description).to eq true
+    end
+
+    it 'returns false if the applicant has pets selected and status is in progress' do
+      FactoryBot.create(:applicant, application: @application)
+      pet = FactoryBot.create(:pet, shelter: FactoryBot.create(:shelter))
+
+      @application.pets << pet
+      @application.status = 'In Progress'
+
+      expect(@application.can_not_edit_description).to eq false
+    end
+
+    it 'returns true if the applicant has no pets selected and status is in progress' do
+      FactoryBot.create(:applicant, application: @application)
+
+      @application.status = 'In Progress'
+
+      expect(@application.can_not_edit_description).to eq true
+    end
   end
 end
