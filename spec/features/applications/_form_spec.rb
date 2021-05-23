3b72f61356a97543e3c7c66c3010c2a6fa84afce
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-23 11:48:21 -0600

    re-write partial feature tests

--- spec/features/applications/_form_spec.rb
+++ spec/features/applications/_form_spec.rb
@@ -1,7 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe 'The partial form for applications,', type: :view do
-  describe 'fields,' do
+
+  after :all do
+    Application.destroy_all
+    Shelter.destroy_all
+  end
+
+  describe 'as a new application,' do
+    before :all do
+      @application = FactoryBot.create(:application)
+    end
+
+    before :each do
+      render partial: 'applications/form', locals: {
+        application: @application,
+        applicant: Applicant.new
+      }
+    end
+
+    it 'has a field to enter a name' do
+      within '#applications_form' do
+        expect(page).to have_content("Applicant's Name")
+        expect(page).to have_field('applicant[name]')
+      end
+    end
+
+    it 'has an address form' do
+      within '#applications_form' do
+        expect(page).to have_selector('#address_form')
+
+        within '#address_form' do
+          expect(page).to have_content('Street:')
+          expect(page).to have_field('address[street]')
+
+          expect(page).to have_content('City:')
+          expect(page).to have_field('address[city]')
+
+          expect(page).to have_content('State:')
+          expect(page).to have_field('address[state]')
+
+          expect(page).to have_content('Zipcode:')
+          expect(page).to have_field('address[zipcode]')
+        end
+      end
+    end
+
+    it 'shows a sumbit button to create application' do
+      within '#applications_form' do
+        expect(page).to have_selector('input', with: 'Create Application')
+      end
+    end
+  end
+
+  describe 'when application is in progress,' do
     before :all do
       @application = FactoryBot.create(:application)
       @applicant = FactoryBot.create(:applicant, application: @application)
@@ -15,43 +67,52 @@ RSpec.describe 'The partial form for applications,', type: :view do
       }
     end
 
-    after :all do
-      Application.destroy_all
-    end
-
-    it 'has an input for an applicants name' do
+    it 'shows the applicant name' do
       within '#applications_form' do
-        expect(page).to have_content("Applicant's Name")
-        expect(page).to have_field('applicant[name]')
+        expect(page).to have_content(@applicant.name)
       end
     end
 
-    it 'has inputs for the applicants address' do
+    it 'shows the address for the applicant' do
       within '#applications_form' do
-        expect(page).to have_content('Street:')
-        expect(page).to have_field('address[street]')
-
-        expect(page).to have_content('City:')
-        expect(page).to have_field('address[city]')
-
-        expect(page).to have_content('State:')
-        expect(page).to have_field('address[state]')
-
-        expect(page).to have_content('Zipcode:')
-        expect(page).to have_field('address[zipcode]')
+        expect(page).to have_selector('#address_description')
       end
     end
 
-    it 'has a description box' do
+    it 'shows a submit button if there are pets on the application' do
       within '#applications_form' do
-        expect(page).to have_content('Descriptive reason why to adopt:')
-        expect(page).to have_field('application[description]')
+        expect(page).to have_selector('input', with: 'Create Application')
       end
     end
 
-    it 'has a submit button' do
-      within '#applications_form' do
-        expect(page).to have_selector("input", type: 'submit')
+    describe 'describe text area,' do
+      it 'is disabled if there are no pets to adopt yet' do
+        application = FactoryBot.create(:application)
+        applicant = FactoryBot.create(:applicant, application: application)
+        FactoryBot.create(:address, applicant: applicant)
+
+        shelter = FactoryBot.create(:shelter)
+        pet = FactoryBot.create(:pet, shelter: shelter)
+
+        application.pets << pet
+        application.status = 'In Progress'
+
+        render partial: 'applications/form', locals: {
+          application: application,
+          applicant: applicant
+        }
+
+        within '#applications_form' do
+          expect(page).to have_content('Why I would make a good home:')
+          expect(page).to have_field('application[description]', disabled: true)
+        end
+      end
+
+      it 'is enabled if there are 1 or more pets to adopt' do
+        within '#applications_form' do
+          expect(page).to have_content('Why I would make a good home:')
+          expect(page).to have_field('application[description]', disabled: false)
+        end
       end
     end
   end
