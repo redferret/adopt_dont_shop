8884dfd5e8f98e007a2ccbf2e848154dfff708a7
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-26 15:13:03 -0600

    Delete unused files with new actions for controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,7 +10,7 @@ class AdminController < ApplicationController
 
   def show_pending_applications
     render 'applications/index', locals: {
-      applications: Application.where(status: 'Pending')
+      applications: Application.order(status: :desc).where.not(status: 'In Progress')
     }
   end
 
@@ -28,6 +28,44 @@ class AdminController < ApplicationController
       back_path: '/admin/applications',
       admin_view: true
     }
+  end
+
+  def approve_pet
+    @pet = Pet.find(params[:id])
+    @application = Application.find(params[:application_id])
+    @pet.update_status_for_application(@application.id, 'approved')
+    review_application
+  end
+
+  def reject_pet
+    @pet = Pet.find(params[:id])
+    @application = Application.find(params[:application_id])
+    @pet.update_status_for_application(@application.id, 'rejected')
+
+    review_application
+  end
+
+  def destroy
+    @application = Application.find(params[:id])
+    @application.destroy
+    flash[:notice] = "Application was successfully destroyed."
+    redirect_to '/admin/applications', locals: {
+      applications: @applications
+    }
+  end
+
+  private
+
+  def review_application
+    status = @application.review_status
+
+    if @application.save
+      flash[:notice] = 'Review Processed'
+      redirect_to "/admin/applications/#{@application.id}"
+    else
+      flash[:alert] = "Something went wrong"
+      redirect_to "/admin/applications/#{@application.id}"
+    end
   end
 
 end
