52c9c1eea5c74274b79cd6b50efe0efbe74c8abc
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-26 12:52:59 -0600

    Implement model methods

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,13 +2,60 @@ class Application < ApplicationRecord
   after_initialize :set_defaults, unless: :persisted?
 
   has_one :applicant, dependent: :destroy
+
   has_and_belongs_to_many :pets
 
   validates :status, presence: true
+  validates :description, presence: true
 
   def set_defaults
     self.description ||= ''
     self.status ||= 'New'
+  end
+
+  def rejected_pet_count
+    ApplicationsPets.where({status: 'rejected', application_id: id}).count
+  end
+
+  def pending_pet_count
+    ApplicationsPets.where({status: 'pending', application_id: id}).count
+  end
+
+  def already_adopted_count
+    pets.where(adoptable:false).count
+  end
+
+  def update_all_pet_statuses(status)
+    ApplicationsPets.where(application_id: id).update_all(status: status)
+  end
+
+  def review_status
+    has_rejections = rejected_pet_count > 0 || already_adopted_count > 0
+    no_rejections = rejected_pet_count == 0
+    no_pending_pets = pending_pet_count == 0
+
+    if has_rejections
+      self.status = 'Rejected'
+      update_all_pet_statuses('waiting')
+      pets.update_all(adoptable: true)
+    elsif no_rejections && no_pending_pets
+      self.status = 'Accepted'
+      pets.update_all(adoptable: false)
+    end
+
+  end
+
+  def status_badge
+    case status
+    when 'In Progress'
+      "<h4><span class='badge badge-info' role='alert'>In Progress</span></h4>".html_safe
+    when 'Pending'
+      "<h4><span class='badge badge-warning' role='alert'>Pending</span></h4>".html_safe
+    when 'Accepted'
+      "<h4><span class='badge badge-success' role='alert'>Accepted!</span></h4>".html_safe
+    when 'Rejected'
+      "<h4><span class='badge badge-danger' role='alert'>Rejected</span></h4>".html_safe
+    end
   end
 
   def is_new?
@@ -21,6 +68,10 @@ class Application < ApplicationRecord
 
   def pending?
     status == 'Pending'
+  end
+
+  def self.all_for_shelter(shelter_id)
+    Application.where(status: 'Pending').joins(:pets).where({pets: {shelter_id: shelter_id}}).group(:id)
   end
 
   def can_not_edit_description
