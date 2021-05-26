ef3864983a3afbadeb6e8b19169a665add0a5e49
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-25 18:35:49 -0600

    Hot fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,36 +4,41 @@
   rand(3..7).times do
     FactoryBot.create(:veterinarian, veterinary_office: vet_office)
   end
-end
+end if VeterinaryOffice.count == 0
 
-5.times do
+8.times do
   shelter = FactoryBot.create(:shelter)
 
-  rand(10..20).times do
+  3.times do
     FactoryBot.create(:pet, shelter: shelter)
   end
-end
+end if Shelter.count == 0
 
-5.times do
+# select shelters starting at first.id to (first.id + 3)
+shelter_id = Shelter.first.id
+3.times do
   application = FactoryBot.create(:application, status: 'In Progress')
   applicant = FactoryBot.create(:applicant, application: application)
   address = FactoryBot.create(:address, applicant: applicant)
 
-  random_shelter = Shelter.all.sample
-  3.times do
+  random_shelter = Shelter.find(shelter_id)
+  shelter_id += 1
+  1.times do
     random_pet = random_shelter.pets.sample
     application.pets << random_pet
   end
-end
+end if Application.count == 0
 
+# select shelters starting at (first.id + 3) to (first.id + 6)
 3.times do
   application = FactoryBot.create(:application, status: 'Pending')
   applicant = FactoryBot.create(:applicant, application: application)
   address = FactoryBot.create(:address, applicant: applicant)
 
-  random_shelter = Shelter.all.sample
-  3.times do
+  random_shelter = Shelter.find(shelter_id)
+  shelter_id += 1
+  1.times do
     random_pet = random_shelter.pets.sample
     application.pets << random_pet
   end
-end
+end if Application.count == 3
