73d01c355a5d9e40026bb88e4abbc4a9628d1d0b
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-27 11:53:50 -0600

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ shelter_id = Shelter.first.id
   2.times do
     random_pet = random_shelter.pets.sample
     application.pets << random_pet if !application.pets.include?(random_pet)
-    ApplicationsPets.where({pet_id: random_pet.id, application_id: application.id}).update_all(status: 'pending')
+    ApplicationsPets.update_status_on(application, random_pet, 'pending')
   end
 end if Application.count == 0
 
@@ -41,6 +41,6 @@ end if Application.count == 0
   2.times do
     random_pet = random_shelter.pets.sample
     application.pets << random_pet if !application.pets.include?(random_pet)
-    ApplicationsPets.where({pet_id: random_pet.id, application_id: application.id}).update_all(status: 'pending')
+    ApplicationsPets.update_status_on(application, random_pet, 'pending')
   end
 end if Application.count == 3
