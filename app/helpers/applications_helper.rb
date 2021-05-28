829fabf198a18d43639f964807ecd7a3818f427c
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-27 21:55:30 -0600

    SRP views

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationsHelper
+  def search_for_pet
+    'applications/partials/pets/search_pets'
+  end
+
+  def list_of_wanted_pets
+    'applications/partials/pets/pets_wanted'
+  end
+
+  def pet_status
+    'applications/partials/pets/pet_status'
+  end
+
+  def application_form
+    'applications/partials/form/form'
+  end
+
+  def pending_application_form
+    'applications/partials/form/form_pending'
+  end
+
+  def new_application_form
+    'applications/partials/form/form_new'
+  end
+
+  def in_progress_application_form
+    'applications/partials/form/form_in_progress'
+  end
+
+  def address_form
+    'applications/partials/applicant/address_form'
+  end
+
+  def address_info
+    'applications/partials/applicant/address'
+  end
 end
